3bd7f86273d0e942cd11d9f844dfc9f46f22f037
Jonathan Falkenstein <jtfalkenstein@gmail.com>
2016-09-25 15:40:37 -0500

    working on getting index to boot app

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+<?php 
+ob_start();
+const DS = DIRECTORY_SEPARATOR;
+const ROOT = __DIR__;
+
+include 'vendor/autoload.php';
+include 'scripts/QMAutoLoader.php';
+include 'scripts/Bootstrapper.php';
+
+
+ob_end_flush();
