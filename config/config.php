1dc3f60fd0ddd7a9d6532279e0e7a6bbc1f1501c
Jonathan Falkenstein <jtfalkenstein@gmail.com>
2016-09-25 16:25:54 -0500

    adding log directory

--- config/config.php
+++ config/config.php
@@ -22,10 +22,12 @@ return [
          * DEBUG;
          * For more info, see https://github.com/katzgrau/KLogger
          */
-       'logFormat' => '[{date}]\t[{level}]\t{message}'
+        'logFormat' => '[{date}]\t[{level}]\t{message}',
+        'logDirectory' => ROOT . DS . 'logs' . DS
         /* 
          * See https://github.com/katzgrau/KLogger for details on configuration.
          */
+        
     ]
     
 ];
