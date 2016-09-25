b0b106d32abd0ce13fef71ecb20d0e1646f320b2
Jonathan Falkenstein <jtfalkenstein@gmail.com>
2016-09-25 14:34:40 -0500

    Converting config json to php

---
+++ config/config.php
@@ -0,0 +1,32 @@
+<?php
+$dataPath = ROOT . DS . 'data' . DS;
+return [
+    'repositories' => [
+        'json' => [
+            'questionsPath' =>  "{$dataPath}questions.json",
+            'testsPath' => "{$dataPath}tests.json",
+            'departments' => "{$dataPath}departments.json"
+        ]
+    ],
+    'logging' => [
+        'logLevel' => "DEBUG",
+        /*
+         * Possible levels, in decreasing priority are:
+         * EMERGENCY;
+         * ALERT;
+         * CRITICAL;
+         * ERROR;
+         * WARNING;
+         * NOTICE;
+         * INFO;
+         * DEBUG;
+         * For more info, see https://github.com/katzgrau/KLogger
+         */
+       'logFormat' => '[{date}]\t[{level}]\t{message}'
+        /* 
+         * See https://github.com/katzgrau/KLogger for details on configuration.
+         */
+    ]
+    
+];
+
