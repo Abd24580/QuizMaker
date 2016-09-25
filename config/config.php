563ea635aad09dcab0edb620fa0cde54a42c6401
Jonathan Falkenstein <jtfalkenstein@gmail.com>
2016-09-25 18:35:22 -0500

    Made log format prettier

--- config/config.php
+++ config/config.php
@@ -22,7 +22,7 @@ return [
          * DEBUG;
          * For more info, see https://github.com/katzgrau/KLogger
          */
-        'logFormat' => '[{date}]\t[{level}]\t{message}',
+        'logFormat' => '[{date}]   [{level}]   {message}',
         'logDirectory' => ROOT . DS . 'logs' . DS
         /* 
          * See https://github.com/katzgrau/KLogger for details on configuration.
