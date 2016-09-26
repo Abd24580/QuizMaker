65db4931ce7657587ccfd0bf3850af700a3fa879
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-09-26 08:24:02 -0500

    Combined bootstrapper with index.php

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php 
 ob_start();
+
+use QM\Application\Application;
+
 const DS = DIRECTORY_SEPARATOR;
 const ROOT = __DIR__;
 
 include 'vendor/autoload.php';
 include 'scripts/QMAutoLoader.php';
-include 'scripts/Bootstrapper.php';
 
-BootErUp();
+$app = new Application();
+$app->Run();
 
 ob_end_flush();
