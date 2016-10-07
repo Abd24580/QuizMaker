9724a997d2b7a22a412c8706b648a2d4f8f47d43
jtfalkenstein <jtfalkenstein@gmail.com>
2016-10-06 21:57:25 -0500

    created department editor handlebars template

---
+++ tmpl/deptEditor.hb.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+ * Copyright (C) 2016 Jon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+
+<div class="deptEditor container">
+    {{#if Id}}
+    <h2>Edit department</h2>
+    {{else}}
+    <h2>Create department</h2>
+    {{/if}}
+    <div class="form-group">
+        <label>
+            Department Name:
+            <input type="text" name="Name" value="{{Name}}">
+        </label>
+    </div>
+</div>
