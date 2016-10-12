4895cf34fc725db0a28b71f1ea015ff61f3c1de5
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-10-12 13:42:14 -0500

    revising partials and helpers for adding questions

--- js/lib/templating.js
+++ js/lib/templating.js
@@ -21,8 +21,9 @@ define([
     'text!templates/questionSection.hbs',
     'text!templates/deptEditor.hbs',
     'text!templates/navbar.hbs',
-    'text!templates/quiz.hbs'
-],function(hb, tQuestion, tQuestionSection, tDeptEdit,tNavBar, tQuiz){
+    'text!templates/quiz.hbs',
+    'text!templates/questionEditor.hbs'
+],function(hb,tQuestion, tQuestionSection, tDeptEdit,tNavBar, tQuiz,tQuestionEditor){
     
     var deptEditorTemplate;
     var quizEditorTemplate;
@@ -32,23 +33,44 @@ define([
         initialize: function (){
             hb.registerPartial('question-partial', tQuestion);
             hb.registerPartial('questionSection-partial', tQuestionSection);
-            
+            hb.registerPartial('questionEditor-partial', tQuestionEditor);
             hb.registerHelper("listAnswers", function(question, options){
                 var answersArray = question.AnswersArray;
                 var lis = [];
+                
+                function push(index, answer){
+                    lis.push(options.fn({index:index, answer:answer, questionId: question.Id}));
+                }
+                
                 for(var a in answersArray){
                     if(a === question.CorrectIndex){
-                        lis.push(options.fn('<strong>' + answersArray[a] + '</strong>'));
+                        push(a, '<strong>' + answersArray[a] + '</strong>' );
                         continue;
                     }
-                    lis.push(options.fn(answersArray[a]));
+                    push(options.fn(answersArray[a]));
                 }
                 return lis.join(' ');
+                
+                
             });
             
             
             hb.registerHelper('listQuestions', function(quiz, options){
-                
+                var lis = [], qos = quiz.QuestionOrders;
+                for(var o in qos){
+                    var question = quiz.Questions[qos[o]];
+                    if(question.editing){
+                        lis.push(options.inverse(question));
+                        continue;
+                    }
+                    lis.push(options.fn(quiz.Questions[qos[o]]));
+                }
+                for(var q in quiz.Questions){
+                    if(q.indexOf('NewQuestion') === 0){
+                        lis.push(options.inverse(quiz.Questions[q]));
+                    }
+                }
+                return lis.join(' ');
             });
         }, 
         get deptEditor(){
