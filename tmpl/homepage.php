d2c82a3f3f984856d29e0694af4e2c4c9dde8740
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-10-13 11:25:57 -0500

    editing homepage

--- tmpl/homepage.php
+++ tmpl/homepage.php
@@ -33,7 +33,9 @@
         <div id="navBarContainer"></div>
         
         <div class="container">
-            <div class="col-sm-8 col-sm-offset-2" id="mainCanvas"></div>
+            <div class="row">
+                <div class="col-lg-8 col-md-offset-2 col-sm-10 col-sm-offset-1" id="mainCanvas"></div>
+            </div>
         </div>
         
     </body>
