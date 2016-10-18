2e33d50ffc196fd718663eb35cad852a236e7376
Jonathan Falkenstein <jfalkenstein@gmail.com>
2016-10-18 12:44:48 -0500

    working on quiz cloner popup

--- js/lib/windows/popUps.js
+++ js/lib/windows/popUps.js
@@ -15,28 +15,45 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-define(['underscore', 'jquery', 'jquery-ui'], function(_,$){
+define(['underscore','quizMaker','templating', 'repository', 'jquery', 'jquery-ui'], function(_,qm,templating,repo,$){
     
     
-    function showQuizCloner(quiz){
-        
+    var modalDialogOptions = {
+        resizable: false,
+        height: 'auto',
+        width: 400,
+        modal: true,
+        show: 400,
+        hide: 400
+    };    
+    
+    
+    function showQuizCloner(){
+        var template = templating.questionCloner;
+        var html = template(qm);
+        var div = $(html);
+        var currentQuiz = qm.currentQuiz;
+        var options = {
+            buttons: [
+                {
+                    text: "Clone!",
+                    click: function(){
+                    }
+                }
+            ]
+        };
+        div.dialog(modalDialogOptions);
     }
     
     
     return {
         showDialog: function(options, dialogText){
-             var div = $('<div class="dialog">' + dialogText + '</div>');
-            var modalDialogOptions = {
-                resizable: false,
-                height: 'auto',
-                width: 400,
-                modal: true,
-                show: 400,
-                hide: 400
-            };
+            var div = $('<div class="dialog">' + dialogText + '</div>');
+            
             var selectedOptions = _.extend(modalDialogOptions, options);
             div.dialog(selectedOptions);      
-        }
+        },
+        showQuizCloner: showQuizCloner
     };
     
 });
